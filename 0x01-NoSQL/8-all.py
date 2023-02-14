edc696f4035fd8b45527ec2d855e316791e1faf1
Marvin Kurland <bugemarvin@outlook.com>
2023-02-14 23:49:53 +0300

    function that lists all documents in a collection:

---
+++ 0x01-NoSQL/8-all.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+'''
+function to list all docs in mongo
+'''
+import pymongo
+
+
+def list_all(mongo_collection):
+    '''
+    print all lists
+    '''
+    details = mongo_collection.find()
+    return [values for values in details]
